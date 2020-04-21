88a0b207c2125daa53de6fe7b65c7dcf1eccec35
arvind kumar <Arvind.Kumar@mrcooper.com>
2020-04-21 22:26:44 +0530

    Privacy doc added

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => (
+  <div className="home">
+    <div className="brand">Program Design Lab</div>
+  </div>
+);
+
+export default Home;
+
+
