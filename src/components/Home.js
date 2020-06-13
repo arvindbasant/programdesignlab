6bf373103ccac83b11840c0e761addf204f8c436
arvind kumar <Arvind.Kumar@mrcooper.com>
2020-06-14 02:50:49 +0530

    text changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,8 @@ import './home.css';
 const Home = () => (
   <div className="home">
     <div className="brand">Program Design Lab</div>
+    <h2>By: Arvind Kumar</h2>
+    <h3>mail: programdesignlab@gmail.com/arvindbasant@outlook.com</h3>
   </div>
 );
 
